f64578087936966fe456735a9bf2334ff73d89e7
Caio Andrade <caio.andrade2442@gmail.com>
2024-05-10 13:53:07 -0300

    style: Better and cleaner code

--- main.py
+++ main.py
@@ -1,309 +1,139 @@
 import random
 
-cont = 0
 
-while True:
-    print("\nVocê deseja jogar em qual língua? (EN/PT)")
-    print("What language do you want to play? (EN/PT)")
-    lang = input("Choose your language: ").upper()
-    match lang:
+def get_language():
+    while True:
+        print("\nVocê deseja jogar em qual língua? (EN/PT)")
+        print("What language do you want to play? (EN/PT)")
+        lang = input("Choose your language: ").upper()
 
-        case "EN":
-            while True:
-                level = input("\nWhich level do you want to play? EASY (1-10) - MEDIUM (1-25) - HARD (1-50): ").upper()
-
-                if level in ["EASY", "MEDIUM", "HARD"]:
-                    break
-                else:
-                    print("Choose a difficulty")
-
-            match level:
-                case "EASY":
-                    print("\nYou are on a number guessing game between 1 - 10 (1 and 10 included).\nGood luck!")
-                    number = random.choice([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-                    # while do jogo
-                    while True:
-                        try:
-                            user_choice = int(input("\nChoose your number: "))
-                        except ValueError:
-                            print("Choose a value as said, number between 1 - 10 (1 and 10 included).")
-                            continue
-
-                        cont += 1
-
-                        if user_choice < 1 or user_choice > 10:
-                            print("Choose a number between 1 - 10")
-                            cont -= 1
-                            continue
-
-                        if user_choice == number:
-                            print("\nCongratulations you got it!")
-                            print(f"Total of tries = {cont}")
-
-                            while True:
-                                user_answer = input("\nDo you want to keep playing? (Y/N): ").upper()
-                                if user_answer in ["Y", "N"]:
-                                    break
-                                else:
-                                    print("Choose Y or N")
-
-                            if user_answer == "Y":
-                                cont = 0
-                                number = random.choice([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-                                continue
-                            else:
-                                print("\nThank you for playing!")
-                                break
-
-                        else:
-                            print("Wrong but keep trying!")
-                            continue
-
-                case "MEDIUM":
-                    print("\nYou are on a number guessing game between 1 - 25 (1 and 25 included).\nGood luck!")
-                    number = random.choice([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25])
-
-                    while True:
-                        try:
-                            user_choice = int(input("\nChoose your number: "))
-                        except ValueError:
-                            print("Choose a value as said, number between 1 - 25 (1 and 25 included).")
-                            continue
-
-                        cont += 1
-
-                        if user_choice < 1 or user_choice > 25:
-                            print("Choose a number between 1 - 25")
-                            cont -= 1
-                            continue
-
-                        if user_choice > number:
-                            print("Lower!")
-
-                        if user_choice < number:
-                            print("Higher!")
-
-                        if user_choice == number:
-                            print("\nCongratulations you got it!")
-                            print(f"Total of tries = {cont}")
-
-                            while True:
-                                user_answer = input("\nDo you want to keep playing? (Y/N): ").upper()
-                                if user_answer in ["Y", "N"]:
-                                    break
-                                else:
-                                    print("Choose Y or N")
-
-                            if user_answer == "Y":
-                                cont = 0
-                                number = random.choice([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25])
-                                continue
-                            else:
-                                print("\nThank you for playing!")
-                                break
-
-                        else:
-                            print("Wrong but keep trying!")
-                            continue
-                case "HARD":
-                    print("\nYou are on a number guessing game between 1 - 50 (1 and 50 included).\nGood luck!")
-                    number = random.choice([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
-                    26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50])
-
-                    while True:
-                        try:
-                            user_choice = int(input("\nChoose your number: "))
-                        except ValueError:
-                            print("Choose a value as said, number between 1 - 50 (1 and 50 included).")
-                            continue
-
-                        cont += 1
-
-                        if user_choice < 1 or user_choice > 50:
-                            print("Choose a number between 1 - 50")
-                            cont -= 1
-                            continue
-
-                        if user_choice > number:
-                            print("Lower!")
-
-                        if user_choice < number:
-                            print("Higher!")
-
-                        if user_choice == number:
-                            print("\nCongratulations you got it!")
-                            print(f"Total of tries = {cont}")
-
-                            while True:
-                                user_answer = input("\nDo you want to keep playing? (Y/N): ").upper()
-                                if user_answer in ["Y", "N"]:
-                                    break
-                                else:
-                                    print("Choose Y or N")
-
-                            if user_answer == "Y":
-                                cont = 0
-                                number = random.choice([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
-                                26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50])
-                                continue
-                            else:
-                                print("\nThank you for playing!")
-                                break
-
-                        else:
-                            print("Wrong but keep trying!")
-                            continue
-
-        case "PT":
-            while True:
-                level = input("\nQual dificuldade deseja jogar? FACIL (1-10) - MEDIO (1-25) - DIFICIL (1-50): ").upper()
-
-                if level in ["FACIL", "MEDIO", "DIFICIL"]:
-                    break
-                else:
-                    print("Escolha uma dificuldade")
-
-            match level:
-                case "FACIL":
-                    print("\nVocê está em um jogo de adivinhação de números entre 1 - 10 (1 e 10 inclusos).\nBoa sorte!")
-                    number = random.choice([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-
-                    while True:
-                        try:
-                            user_choice = int(input("\nEscolha seu número: "))
-                        except ValueError:
-                            print("Escolha um valor como foi dito, número entre 1 - 10 (1 e 10 inclusos).")
-                            continue
-
-                        cont += 1
-
-                        if user_choice < 1 or user_choice > 10:
-                            print("Escolha um número de 1 até 10!")
-                            cont -= 1
-                            continue
-
-                        if user_choice == number:
-                            print("\nParabêns você acertou!")
-                            print(f"Total de tentativas = {cont}")
-
-                            while True:
-                                user_answer = input("\nVocê deseja continuar jogando? (S/N): ").upper()
-                                if user_answer in ["S", "N"]:
-                                    break
-                                else:
-                                    print("Escolha S ou N")
-
-                            if user_answer == "S":
-                                cont = 0
-                                number = random.choice([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-                                continue
-                            else:
-                                print("\nObrigado por jogar!")
-                                break
-
-                        else:
-                            print("Errou mas continue tentando!")
-                            continue
-
-                case "MEDIO":
-                    print("\nVocê está em um jogo de adivinhação de números entre 1 - 25 (1 e 25 inclusos).\nBoa sorte!")
-                    number = random.choice([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25])
-
-                    while True:
-                        try:
-                            user_choice = int(input("\nEscolha seu número: "))
-                        except ValueError:
-                            print("Escolha um valor como foi dito, número entre 1 - 25 (1 e 25 inclusos).")
-                            continue
-
-                        cont += 1
-
-                        if user_choice < 1 or user_choice > 25:
-                            print("Escolha um número de 1 até 25!")
-                            cont -= 1
-                            continue
-
-                        if user_choice > number:
-                            print("Menor!")
-
-                        if user_choice < number:
-                            print("Maior!")
-
-                        if user_choice == number:
-                            print("\nParabêns você acertou!")
-                            print(f"Total de tentativas = {cont}")
-
-                            while True:
-                                user_answer = input("\nVocê deseja continuar jogando? (S/N): ").upper()
-                                if user_answer in ["S", "N"]:
-                                    break
-                                else:
-                                    print("Escolha S ou N")
-
-                            if user_answer == "S":
-                                cont = 0
-                                number = random.choice([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25])
-                                continue
-                            else:
-                                print("\nObrigado por jogar!")
-                                break
-
-                        else:
-                            print("Errou mas continue tentando!")
-                            continue
-
-                case "DIFICIL":
-                    print("\nVocê está em um jogo de adivinhação de números entre 1 - 50 (1 e 50 inclusos).\nBoa sorte!")
-                    number = random.choice([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
-                    26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50])
-
-                    while True:
-                        try:
-                            user_choice = int(input("\nEscolha seu número: "))
-                        except ValueError:
-                            print("Escolha um valor como foi dito, número entre 1 - 50 (1 e 50 inclusos).")
-                            continue
-
-                        cont += 1
-
-                        if user_choice < 1 or user_choice > 50:
-                            print("Escolha um número de 1 até 50!")
-                            cont -= 1
-                            continue
-
-                        if user_choice > number:
-                            print("Menor!")
-
-                        if user_choice < number:
-                            print("Maior!")
-
-                        if user_choice == number:
-                            print("\nParabêns você acertou!")
-                            print(f"Total de tentativas = {cont}")
-
-                            while True:
-                                user_answer = input("\nVocê deseja continuar jogando? (S/N): ").upper()
-                                if user_answer in ["S", "N"]:
-                                    break
-                                else:
-                                    print("Escolha S ou N")
-
-                            if user_answer == "S":
-                                cont = 0
-                                number = random.choice([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,23, 24, 25,
-                                26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50])
-                                continue
-                            else:
-                                print("\nObrigado por jogar!")
-                                break
-
-                        else:
-                            print("Errou mas continue tentando!")
-                            continue
-
-        case _:
-            print("\nEscolha uma lingugagem como foi dita.")
+        if lang in ["EN", "PT"]:
+            return lang
+        else:
             print("Choose a language as said.")
+
+
+def get_difficulty(lang):
+    while True:
+        if lang == "EN":
+            level = input("\nWhich level do you want to play? EASY (1-10) - MEDIUM (1-25) - HARD (1-50): ").upper()
+        else:
+            level = input("\nQual dificuldade deseja jogar? FACIL (1-10) - MEDIO (1-25) - DIFICIL (1-50): ").upper()
+
+        if level in ["EASY", "MEDIUM", "HARD", "FACIL", "MEDIO", "DIFICIL"]:
+            return level
+        else:
+            print("Choose a difficulty.")
+
+
+def generate_number(level):
+    if level in ["EASY", "FACIL"]:
+        return random.randint(1, 10)
+    elif level in ["MEDIUM", "MEDIO"]:
+        return random.randint(1, 25)
+    elif level in ["HARD", "DIFICIL"]:
+        return random.randint(1, 50)
+
+
+def get_user_input(level):
+    while True:
+        try:
+            user_choice = int(input("\nChoose your number: "))
+            if level in ["EASY", "FACIL"]:
+                valid_range = range(1, 11)
+            elif level in ["MEDIUM", "MEDIO"]:
+                valid_range = range(1, 26)
+            else:
+                valid_range = range(1, 51)
+
+            if user_choice not in valid_range:
+                print(f"Choose a value as said, number between 1 - {50 if level in ['HARD', 'DIFICIL'] else 25 if level in ['MEDIUM', 'MEDIO'] else 10} (1 and {50 if level in ['HARD', 'DIFICIL'] else 25 if level in ['MEDIUM', 'MEDIO'] else 10} included).")
+                continue
+
+        except ValueError:
+            print(f"Choose a value as said, number between 1 - {50 if level in ['HARD', 'DIFICIL'] else 25 if level in ['MEDIUM', 'MEDIO'] else 10} (1 and {50 if level in ['HARD', 'DIFICIL'] else 25 if level in ['MEDIUM', 'MEDIO'] else 10} included).")
             continue
 
-    break
+        return user_choice
+
+
+def get_game_info(lang, level):
+    range_text = f"1 - {50 if level in ['HARD', 'DIFICIL'] else 25 if level in ['MEDIUM', 'MEDIO'] else 10}"
+    range_inclusive = f"1 and {50 if level in ['HARD', 'DIFICIL'] else 25 if level in ['MEDIUM', 'MEDIO'] else 10} included"
+
+    if lang == "EN":
+        return f"\nYou are on a number guessing game between {range_text} ({range_inclusive}).\nGood luck!"
+    else:
+        return f"\nVocê está em um jogo de adivinhação de números entre {range_text} (1 e {range_inclusive} inclusos).\nBoa sorte!"
+
+
+def get_congratulatory_message(lang):
+    if lang == "EN":
+        return "\nCongratulations you got it!"
+    else:
+        return "\nVocê conseguiu parabéns!"
+
+
+def get_continue_message(lang):
+    if lang == "EN":
+        return "\nDo you want to keep playing? (Y/N): "
+    else:
+        return "\nDeseja continuar jogando? (S/N): "
+
+
+def get_wrong_guess_message(lang, user_choice, number):
+    if lang == "EN":
+        return ("Lower!" if user_choice > number else "Higher!") + "\nWrong but keep trying!"
+    else:
+        return ("Menor!" if user_choice > number else "Maior!") + "\nErrou mas continue tentando"
+
+
+def get_thank_you_message(lang):
+    if lang == "EN":
+        return "\nThank you for playing!"
+    else:
+        return "\nObrigado por jogar!"
+
+
+def play_game(lang, level):
+    print(get_game_info(lang, level))
+
+    number = generate_number(level)
+    cont = 0
+
+    while True:
+        user_choice = get_user_input(level)
+        cont += 1
+
+        if user_choice == number:
+            print(get_congratulatory_message(lang))
+            print(f"Total de tentativas = {cont}")
+
+            while True:
+                user_answer = input(get_continue_message(lang)).upper()
+                if user_answer in ["Y", "N"]:
+                    break
+                else:
+                    print("Choose Y or N")
+
+            if user_answer == "Y":
+                return True
+            else:
+                print(get_thank_you_message(lang))
+                return False
+
+        else:
+            print(get_wrong_guess_message(lang, user_choice, number))
+
+
+def main():
+    lang = get_language()
+
+    while True:
+        level = get_difficulty(lang)
+        if not play_game(lang, level):
+            break
+
+
+if __name__ == "__main__":
+    main()
